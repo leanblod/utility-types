6b4ac27490a5d93c38df34d7b4c9c8261c174aa5
Leandro Blodorn <leandro.blodorn@grupotuportal.com>
2025-02-01 14:04:25 -0300

    Changed format to be compliant with JSON.stringify's output

--- lib/json.d.ts
+++ lib/json.d.ts
@@ -2,23 +2,23 @@ import type { Keys } from '../lib';
 export type { JsonStringify as Stringify, JsonParsable as Value };
 
 type JsonStringify<JS extends JsonParsable> =
-    JS extends JsonObject ? ParsedJsonObject<JS>
-    : JS extends JsonArray ? ParsedJsonArray<JS>
+    JS extends JsonArray ? ParsedJsonArray<JS>
+    : JS extends JsonObject ? ParsedJsonObject<JS>
     : JS extends JsonLiterals ? `${JS}`
     : JS extends JsonString ? `"${JS}"` : never;
 
 // Allowed JS types for JSON parsing
-type JsonParsable = JsonObject | JsonArray | JsonLiterals | JsonString;
-type JsonObject = { [key: string]: JsonParsable; };
+type JsonParsable = JsonArray | JsonObject | JsonLiterals | JsonString;
 type JsonArray = JsonParsable[];
+type JsonObject = { [key: string]: JsonParsable; };
 type JsonLiterals = boolean | null | number;
 type JsonString = string;
 
 //@ts-ignore
-type ParsedJsonObject<JS extends JsonObject> = `{ ${
+type ParsedJsonObject<JS extends JsonObject> = `{${
     //@ts-ignore
     ParsedJsonObjectRecursive<JS, Keys.ToTuple<JS>>
-    } }`;
+    }}`;
 
 type ParsedJsonObjectRecursive<
     O extends { [key: string]: JsonParsable },
@@ -26,17 +26,14 @@ type ParsedJsonObjectRecursive<
 > = Keys extends [
     infer NextKey extends `${string | number & keyof O}`,
     ...infer RestKeys extends Keys[number][]
-] ? `"${NextKey}": ${JsonStringify<O[NextKey]>}${RestKeys extends [] ?
-'' : `, ${ParsedJsonObjectRecursive<O, RestKeys>}`
+] ? `"${NextKey}":${JsonStringify<O[NextKey]>}${RestKeys extends [] ?
+'' : `,${ParsedJsonObjectRecursive<O, RestKeys>}`
 }` : '';
 
-type ParsedJsonArray<A extends JsonArray> = `[ ${ParsedJsonArrayRecursive<A>} ]`;
+type ParsedJsonArray<A extends JsonArray> = `[${ParsedJsonArrayRecursive<A>}]`;
 type ParsedJsonArrayRecursive<A extends JsonArray> = A extends [
     infer NextItem extends JsonParsable,
     ...infer RestItems extends JsonArray,
 ] ? `${JsonStringify<NextItem>}${RestItems extends [] ?
-'' : `, ${ParsedJsonArrayRecursive<RestItems>}`
+'' : `,${ParsedJsonArrayRecursive<RestItems>}`
 }` : '';
-
-// TODO - Parser for JSON string literal
-// type JsonParse<Json extends string> = Json extends JsonStringify<infer JS extends JsonParsable> ? JS : never;
