4ccd039dbd92280959bdc7a00c6e34bd6693f06f
Leandro Blodorn <leandro.blodorn@grupotuportal.com>
2025-02-05 12:19:20 -0300

    Added some helper types and its documentation

--- lib/utils.d.ts
+++ lib/utils.d.ts
@@ -1,1 +1,25 @@
+/**
+ * It maps to the `Else` generic if the `SomeType` generic is `never`,
+ * otherwise it maps to `SomeType`
+ */
 export type IfNeverElse<SomeType, Else> = [SomeType] extends [never] ? Else : SomeType;
+
+/**
+ * Maps to a union of objects, that each require a different property of `T`,
+ * and admit the rest as optionals.
+ * If passed `E`, also excludes those types from the required properties, it's
+ * useful to remove nullish values form nullable properties.
+ */
+export type AtLeastOne<T, E extends T[keyof T] = never> = Partial<T> & OnlyOne<T, E>;
+
+/**
+ * Maps to a union of objects, that each require a different property of `T`
+ * and only that one property.
+ * If passed `E`, also excludes those types from the required properties, it's
+ * useful to remove nullish values form nullable properties.
+ */
+export type OnlyOne<T, E extends T[keyof T] = never> = {
+    [K in keyof T]-?: {
+        [IK in K]-?: Exclude<T[IK], E>;
+    };
+}[keyof T];
